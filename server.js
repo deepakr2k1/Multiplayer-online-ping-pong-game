ab8e7548dcd83085de4162a2fe260c8dec855651
DEEPAK <deepakrathore2k1@gmail.com>
2021-01-01 21:26:22 +0530

    V-1.0.1

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ io.on('connection', (socket) => {
     })
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 http.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
